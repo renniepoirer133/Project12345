d6dc19b5395594beea94f5ac67d695fae22f1640
renniepoirer133 <renniepoirer133@gmail.com>
2025-03-07 16:55:02 +0000

    Add 2.js

---
+++ src/2.js
@@ -0,0 +1,7 @@
+function getRandomNumber() {
+  const min = 1;
+  const max = 100;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log("Random number:", getRandomNumber());
